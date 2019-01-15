0c6ddd86696eebe6e821a77a27ca64b4348dfdbf
joetechem <joe@techemstudios.com>
2019-01-14 23:38:28 -0500

    card output doesn't like & symbol

--- src/src.py
+++ src/src.py
@@ -135,11 +135,11 @@ def events_ticketing_services(intent, session):
     speech_output = ""
     should_end_session = True
     
-    card_output = "We manage and promote dozens of events each year and offer clients a method for selling tickets and promoting events."
+    card_output = "Check out CoVaTIX.com and HillCityTIX.com | We manage and promote dozens of events each year and offer clients a method for selling tickets and promoting events."
     speech_output = "<speak>Through our Events and Ticketing Services <break time=\"0.25s\"/> " + services_list[3] + " </speak>"
 
     return build_response(session_attributes, build_speechlet_response
-                          ("Events & Ticketing Services", card_output, speech_output, reprompt_text, should_end_session))
+                          ("Events and Ticketing Services", card_output, speech_output, reprompt_text, should_end_session))
 
 
 # List of example testimonials to return when testimonials() is called
